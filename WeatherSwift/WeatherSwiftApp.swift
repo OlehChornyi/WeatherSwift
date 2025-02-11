4ca6db78e4774ccec9bb595fe589efb6aac04950
oleh <phd.chornyi@gmail.com>
2025-02-11 10:15:51 +0200

    Initial Commit

---
+++ WeatherSwift/WeatherSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftApp.swift
+//  WeatherSwift
+//
+//  Created by Oleh on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
